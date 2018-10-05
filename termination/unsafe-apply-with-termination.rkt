4baf3611bb9b52b070dd5593764831570f7cefad
Phil Nguyen <pcn@cs.umd.edu>
2018-10-05 16:13:09 -0400

    reduce duplicate code

--- termination/unsafe-apply-with-termination.rkt
+++ termination/unsafe-apply-with-termination.rkt
@@ -19,121 +19,84 @@
   #:mutable
   #:transparent)
 
-;; hack just to see limit
-(require racket/pretty)
-(define-syntax dynamic-wind
+(define-syntax with-pre-and-post
   (syntax-rules (λ)
     [(_ (λ () e₁ ...) exec (λ () e₂ ...))
      (let ()
        e₁ ...
        (begin0 (exec)
-         e₂ ...))]))
+         e₂ ...))]
+    ;; ideally, but slow
+    #;[(_ pre exec post) (dynamic-wind pre exec post)]))
+
+(define-syntax-rule (with-pre pre exec _)
+  (begin (pre) (exec)))
 
 (define call-stack ((inst make-hasheq Procedure (U #t (MPairof Positive-Integer Record)))))
 
 (define (divergence-ok?) (hash-empty? call-stack))
 
-(: apply/guard/restore (∀ (X Y) (X * → Y) X * → Y))
-(define (apply/guard/restore f . xs)
-  (define (exec) (apply f xs))
+(define-syntax-rule (define-apply app wrapper)
+  (begin
+    (: app (∀ (X Y) (X * → Y) X * → Y))
+    (define (app f . xs)
+      (define (exec) (apply f xs))
 
-  (define (exec/mark-seen)
-    (dynamic-wind
-      (λ () (hash-set! call-stack f #t))
-      exec
-      (λ () (hash-remove! call-stack f))))
+      (define (exec/mark-seen)
+        (wrapper
+          (λ () (hash-set! call-stack f #t))
+          exec
+          (λ () (hash-remove! call-stack f))))
 
-  (define (exec/mark-first-loop)
-    (dynamic-wind
-      (λ () (hash-set! call-stack f ((inst mcons Positive-Integer Record) 1 (Record xs (list (init-sc-graph (length xs)))))))
-      exec
-      (λ () (hash-set! call-stack f #t))))
+      (define (exec/mark-first-loop)
+        (wrapper
+          (λ () (hash-set! call-stack f ((inst mcons Positive-Integer Record) 1 (Record xs (list (init-sc-graph (length xs)))))))
+          exec
+          (λ () (hash-set! call-stack f #t))))
 
-  (: exec/bump-loop-count : (MPairof Positive-Integer Record) Positive-Integer Positive-Integer → Y)
-  (define (exec/bump-loop-count rec n₀ n)
-    (dynamic-wind
-      (λ () (set-mcar! rec n))
-      exec
-      (λ () (set-mcar! rec n₀))))
+      (: exec/bump-loop-count : (MPairof Positive-Integer Record) Positive-Integer Positive-Integer → Y)
+      (define (exec/bump-loop-count rec n₀ n)
+        (wrapper
+          (λ () (set-mcar! rec n))
+          exec
+          (λ () (set-mcar! rec n₀))))
 
-  (: exec/bump-loop-count-and-check : (MPairof Positive-Integer Record) Positive-Integer Positive-Integer → Y)
-  (define (exec/bump-loop-count-and-check rec n₀ n)
-    (define r (mcdr rec))
-    (define xs₀ (Record-last-examined-args r))
-    (define Gs₀ (Record-last-sc-graph r))
-    (dynamic-wind
-      (λ ()
-        (define Gs (update-record r f xs))
-        (set-mcar! rec n)
-        (set-Record-last-examined-args! r xs)
-        (set-Record-last-sc-graph! r Gs))
-      exec
-      (λ ()
-        (set-mcar! rec n₀)
-        (set-Record-last-examined-args! r xs₀)
-        (set-Record-last-sc-graph! r Gs₀))))
-  (cond
-    ;; `f` detected as loop entry
-    [(hash-ref call-stack f #f)
-     =>
-     (λ (rec)
-       (if (mpair? rec)
-           ;; `f` loops back second time onwards
-           (let* ([n₀ (mcar rec)]
-                  [n (add1 n₀)])
-             ;; bump loop count, check against SCT violation at 2ⁿ-th iterations
-             (if (zero? (unsafe-fxand n n₀))
-                 (exec/bump-loop-count-and-check rec n₀ n)
-                 (exec/bump-loop-count rec n₀ n)))
-           ;; `f` loops back first time
-           (exec/mark-first-loop)))]
-    ;; `f` is seen first time in this call chain
-    [else (exec/mark-seen)]))
+      (: exec/bump-loop-count-and-check : (MPairof Positive-Integer Record) Positive-Integer Positive-Integer → Y)
+      (define (exec/bump-loop-count-and-check rec n₀ n)
+        (define r (mcdr rec))
+        (define xs₀ (Record-last-examined-args r))
+        (define Gs₀ (Record-last-sc-graph r))
+        (wrapper
+         (λ ()
+           (define Gs (update-record r f xs))
+           (set-mcar! rec n)
+           (set-Record-last-examined-args! r xs)
+           (set-Record-last-sc-graph! r Gs))
+         exec
+         (λ ()
+           (set-mcar! rec n₀)
+           (set-Record-last-examined-args! r xs₀)
+           (set-Record-last-sc-graph! r Gs₀))))
+      (cond
+        ;; `f` detected as loop entry
+        [(hash-ref call-stack f #f)
+         =>
+         (λ (rec)
+           (if (mpair? rec)
+               ;; `f` loops back second time onwards
+               (let* ([n₀ (mcar rec)]
+                      [n (add1 n₀)])
+                 ;; bump loop count, check against SCT violation at 2ⁿ-th iterations
+                 (if (zero? (unsafe-fxand n n₀))
+                     (exec/bump-loop-count-and-check rec n₀ n)
+                     (exec/bump-loop-count rec n₀ n)))
+               ;; `f` loops back first time
+               (exec/mark-first-loop)))]
+        ;; `f` is seen first time in this call chain
+        [else (exec/mark-seen)]))))
 
-(: apply/guard : (∀ (X Y) (X * → Y) X * → Y))
-;; FIXME: Shameless duplicate of `apply/guard/restore`
-(define (apply/guard f . xs)
-  (define (exec) (apply f xs))
-
-  (define (exec/mark-seen)
-    (hash-set! call-stack f #t)
-    (exec))
-
-  (define (exec/mark-first-loop)
-    (hash-set! call-stack f ((inst mcons Positive-Integer Record) 1 (Record xs (list (init-sc-graph (length xs))))))
-    (exec))
-
-  (: exec/bump-loop-count : (MPairof Positive-Integer Record) Positive-Integer → Y)
-  (define (exec/bump-loop-count rec n)
-    (set-mcar! rec n)
-    (exec))
-
-  (: exec/bump-loop-count-and-check : (MPairof Positive-Integer Record) Positive-Integer → Y)
-  (define (exec/bump-loop-count-and-check rec n)
-    (define r (mcdr rec))
-    (define Gs (update-record r f xs))
-    (set-mcar! rec n)
-    (set-Record-last-examined-args! r xs)
-    (set-Record-last-sc-graph! r Gs)
-    (exec))
-
-  (cond
-    ;; `f` detected as loop entry
-    [(hash-ref call-stack f #f)
-     =>
-     (λ (rec)
-       (if (mpair? rec)
-           ;; `f` loops back second time onwards
-           (let* ([n₀ (mcar rec)]
-                  [n (add1 n₀)])
-             ;; bump loop count, check against SCT violation at 2ⁿ-th iterations
-             (if (zero? (unsafe-fxand n n₀))
-                 (exec/bump-loop-count-and-check rec n)
-                 (exec/bump-loop-count rec n)))
-           ;; `f` loops back first time
-           (exec/mark-first-loop)))]
-    ;; `f` is seen first time in this call chain
-    [else (exec/mark-seen)]))
+(define-apply apply/guard/restore with-pre-and-post)
+(define-apply apply/guard with-pre)
 
 (: update-record : Record Procedure (Listof Any) → (Pairof SC-Graph (Listof SC-Graph)))
 (define (update-record r f xs)
